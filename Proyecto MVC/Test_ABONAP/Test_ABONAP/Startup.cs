4dfc8493ed719dce0f3c1bc6f1b4b7d18426c14f
Derick <derick290801@gmail.com>
2022-07-14 23:56:21 -0400

    Empresas controller and view completed

--- Proyecto MVC/Test_ABONAP/Test_ABONAP/Startup.cs
+++ Proyecto MVC/Test_ABONAP/Test_ABONAP/Startup.cs
@@ -24,6 +24,7 @@ namespace Test_ABONAP
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddScoped<ABONAPContext, ABONAPContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
