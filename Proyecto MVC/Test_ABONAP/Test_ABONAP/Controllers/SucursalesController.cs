8fee49f4b70c4654c30cf3402bf4868eec39edea
Derick <derick290801@gmail.com>
2022-07-15 00:01:23 -0400

    Sucursales controller and views completed

---
+++ Proyecto MVC/Test_ABONAP/Test_ABONAP/Controllers/SucursalesController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Test_ABONAP;
+
+namespace Test_ABONAP.Controllers
+{
+    public class SucursalesController : Controller
+    {
+        private readonly ABONAPContext _context;
+
+        public SucursalesController(ABONAPContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Sucursales
+        public async Task<IActionResult> Index()
+        {
+            var aBONAPContext = _context.Sucursales.Include(s => s.IdEmpresaNavigation);
+            return View(await aBONAPContext.ToListAsync());
+        }
+
+        // GET: Sucursales/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sucursale = await _context.Sucursales
+                .Include(s => s.IdEmpresaNavigation)
+                .FirstOrDefaultAsync(m => m.IdSucursal == id);
+            if (sucursale == null)
+            {
+                return NotFound();
+            }
+
+            return View(sucursale);
+        }
+
+        // GET: Sucursales/Create
+        public IActionResult Create()
+        {
+            ViewData["IdEmpresa"] = new SelectList(_context.Empresas, "IdEmpresa", "IdEmpresa");
+            return View();
+        }
+
+        // POST: Sucursales/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdSucursal,Descripcion,FechaCreacion,FechaActualizacion,Codigo,Estado,IdEmpresa")] Sucursale sucursale)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(sucursale);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdEmpresa"] = new SelectList(_context.Empresas, "IdEmpresa", "IdEmpresa", sucursale.IdEmpresa);
+            return View(sucursale);
+        }
+
+        // GET: Sucursales/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sucursale = await _context.Sucursales.FindAsync(id);
+            if (sucursale == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdEmpresa"] = new SelectList(_context.Empresas, "IdEmpresa", "IdEmpresa", sucursale.IdEmpresa);
+            return View(sucursale);
+        }
+
+        // POST: Sucursales/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdSucursal,Descripcion,FechaCreacion,FechaActualizacion,Codigo,Estado,IdEmpresa")] Sucursale sucursale)
+        {
+            if (id != sucursale.IdSucursal)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(sucursale);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SucursaleExists(sucursale.IdSucursal))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdEmpresa"] = new SelectList(_context.Empresas, "IdEmpresa", "IdEmpresa", sucursale.IdEmpresa);
+            return View(sucursale);
+        }
+
+        // GET: Sucursales/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sucursale = await _context.Sucursales
+                .Include(s => s.IdEmpresaNavigation)
+                .FirstOrDefaultAsync(m => m.IdSucursal == id);
+            if (sucursale == null)
+            {
+                return NotFound();
+            }
+
+            return View(sucursale);
+        }
+
+        // POST: Sucursales/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var sucursale = await _context.Sucursales.FindAsync(id);
+            _context.Sucursales.Remove(sucursale);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SucursaleExists(int id)
+        {
+            return _context.Sucursales.Any(e => e.IdSucursal == id);
+        }
+    }
+}
